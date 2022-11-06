22384716509e618273aa8ac3d6a0029b6439c213
Xert <xertdev@gmail.com>
2022-11-06 11:38:02 +0100

    :sparkles:Session key service

---
+++ include/schema_type.hpp
@@ -0,0 +1,12 @@
+#ifndef HERDSMAN_SCHEMA_TYPE_HPP
+#define HERDSMAN_SCHEMA_TYPE_HPP
+
+#include <cstdint>
+
+
+enum class SchemaType: uint8_t
+{
+	BINFHE
+};
+
+#endif //HERDSMAN_SCHEMA_TYPE_HPP
---
+++ src/service/key_service.cpp
@@ -0,0 +1,87 @@
+#include "service/key_service.hpp"
+
+#include "spdlog/spdlog.h"
+
+#include "service/common_exceptions.hpp"
+#include "utils/file_utils.hpp"
+
+
+namespace fs = std::filesystem;
+
+
+void KeyService::add_key(const UUID& session_uuid, SchemaType type, const std::vector<std::byte>& key_data)
+{
+	if(!keys_.contains(session_uuid))
+	{
+		create_directory_for_session(session_uuid);
+	}
+
+	const auto session_dir_path = key_storage_dir_ / session_uuid.as_string();
+	const auto type_name = std::to_string(static_cast<std::underlying_type_t<SchemaType>>(type)) + ".key";
+
+	const auto key_path = session_dir_path / type_name;
+
+	if(const auto key_file_status = fs::status(key_path); fs::is_regular_file(key_file_status))
+	{
+		spdlog::info("Found old schema {} key file for schema {}. Old file will be overwritten.", type_name, session_uuid.as_string());
+	}
+
+	write_file(key_path, key_data);
+
+	keys_.emplace(
+			session_uuid,
+			KeyEntry{type, key_path}
+    );
+}
+
+void KeyService::create_directory_for_session(const UUID& uuid) const
+{
+	const auto session_dir_path = key_storage_dir_ / uuid.as_string();
+	const auto session_dir_status = fs::status(session_dir_path);
+
+	if(fs::is_directory(session_dir_status))
+	{
+		spdlog::info("Clearing old directory for session {}", uuid.as_string());
+		std::filesystem::remove_all(session_dir_path);
+	}
+	else
+	{
+		fs::create_directory(session_dir_path);
+		fs::permissions(session_dir_path, fs::perms::owner_all, fs::perm_options::replace);
+
+		spdlog::info("Created new directory for session {}", uuid.as_string());
+	}
+}
+
+void KeyService::remove_key(const UUID& session_uuid, SchemaType type)
+{
+	const auto [keys_begin, keys_end] = keys_.equal_range(session_uuid);
+	const auto key_with_type_predicate = [type](const auto& key_entry)
+	{
+		return key_entry.second.type == type;
+	};
+
+	if(const auto key_iter = std::find_if(keys_begin, keys_end,  key_with_type_predicate); key_iter != keys_end)
+	{
+		fs::remove(key_iter->second.key_path);
+	}
+	else
+	{
+		throw ObjectNotFoundException("No key of selected type assigned to session");
+	}
+}
+
+std::vector<SchemaType> KeyService::list_available_keys(const UUID& session_uuid) const
+{
+	std::vector<SchemaType> types;
+	const auto [keys_begin, keys_end] = keys_.equal_range(session_uuid);
+
+	std::transform(keys_begin, keys_end, std::back_inserter(types),
+				   [](const auto& entry)
+				   {
+					   return entry.second.type;
+				   }
+   );
+
+	return types;
+}
